cc3aeb8c4c562e68fe5ce7abb9098e626585c8c1
milordplus <milordplus@mail.ru>
2023-10-14 23:13:46 +0300

    Гоша Дударь shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+import Categories from "./components/Categories";
+import Details from "./components/Details";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: [],
+            currentItems: [],
+            items: [
+                {
+                    id: 1,
+                    title: 'Стул серый',
+                    img: 'chair-grey.jpeg',
+                    desc: 'Hello world',
+                    category: 'chairs',
+                    price: '49.99'
+                },
+                {
+                    id: 2,
+                    title: 'Стол',
+                    img: 'table.webp',
+                    desc: 'Hello world',
+                    category: 'tables',
+                    price: '129.00'
+                },
+                {
+                    id: 3,
+                    title: 'Диван',
+                    img: 'sofa.jpeg',
+                    desc: 'Hello world',
+                    category: 'sofa',
+                    price: '33.00'
+                },
+                {
+                    id: 4,
+                    title: 'Лампа',
+                    img: 'wall-light.jpeg',
+                    desc: 'Hello world',
+                    category: 'light',
+                    price: '25.50'
+                },
+                {
+                    id: 5,
+                    title: 'Стул белый',
+                    img: 'chair-white.jpeg',
+                    desc: 'Hello world',
+                    category: 'chairs',
+                    price: '50.99'
+                }
+            ],
+            details: false,
+            fullItem: {}
+        }
+        this.state.currentItems = this.state.items
+        this.addToOrder = this.addToOrder.bind(this)
+        this.deleteOrder = this.deleteOrder.bind(this)
+        this.chooseCategory = this.chooseCategory.bind(this)
+        this.onDetails = this.onDetails.bind(this)
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
+                <Categories chooseCategory={this.chooseCategory}/>
+                <Items onDetails={this.onDetails} items={this.state.currentItems} onAdd={this.addToOrder}/>
+
+                {this.state.details && <Details onAdd={this.addToOrder} onDetails={this.onDetails} item={this.state.fullItem}/>}
+                <Footer/>
+            </div>
+        );
+    }
+
+    onDetails(item) {
+        this.setState({fullItem: item})
+        this.setState({details: !this.state.details})
+    }
+
+    chooseCategory(category) {
+        if (category === 'all') {
+            this.setState({currentItems: this.state.items})
+            return
+        }
+        this.setState({
+            currentItems: this.state.items.filter(el => el.category === category)
+        });
+    }
+
+    addToOrder(item) {
+        let isInArray = false
+        this.state.orders.forEach(el => {
+            if (el.id === item.id)
+                isInArray = true
+        })
+        if (!isInArray)
+            this.setState({orders: [...this.state.orders, item]})
+    }
+
+    deleteOrder(id) {
+        this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+    }
 }
 
 export default App;
